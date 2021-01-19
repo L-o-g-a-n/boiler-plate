7a997ccc2802e1ba7d46068959febb436c2475e5
L-o-g-a-n <xy.logandev@gmail.com>
2021-01-19 17:34:57 +0900

    test

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://logan:gsgs1212@boilerplate.wbkfm.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false 
+}).then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello 안녕하세요!!???'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+ 
