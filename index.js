76fc08fabb7de42638cb29362ef62d1714bb494d
L-o-g-a-n <xy.logandev@gmail.com>
2021-01-22 15:14:03 +0900

    adding middleware

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 const config = require('./config/key');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 
 
 
@@ -23,7 +25,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello 안녕'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
     //회원가입할 때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
@@ -38,6 +40,67 @@ app.post('/register', (req, res) => {
     })
 })
 
+
+
+app.post('/api/users/login', (req, res) => {
+    //요청된 이메일이 데이터 베이스에 있는지 확인한다.
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            }) 
+        }
+
+        //이메일이있다면, 비밀번호가 맞는지 확인한다.
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+
+            //비밀번호까지 맞다면, 토큰을 생성하기.
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);   
+            
+                //토큰을 저장한다. 어디에? 쿠키, 로컬 스토리지 ! 여기선 쿠키에 저장하겠다.
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({ loginSuccess: true, userId: user._id})
+
+            })
+        })
+    })
+})
+
+
+app.get('/api/users/auth', auth, (req, res) => {
+    //여기까지 미들웨어를 통과해 왔다는 얘기는, Authentication이 true라는 말.
+
+
+    //role=> 0이면, 일반유저, role=> 0이 아니면 관리자로 설정해놓은 것!
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ?  false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+
+})  
+
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id },
+        { token: ""}
+    , (err, user) => {
+        if(err) return res.json({ success: false, err});
+        return res.status(200).send({
+            success: true
+        })
+    })
+})
 
 
 
